70523dc1ef6e6b476beeaac22c41f51cce60c4a8
rafaelmacario <rafaelmacario@gmail.com>
2013-02-08 05:20:40 -0800

    Extrator de paginas de pdf

---
+++ pdfextract.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Extrai paginas de um pdf
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Default Header and Footer
+ * @author Rafael Macário Bolina
+ * @since 2013-02-07
+ */
+
+date_default_timezone_set('America/Sao_Paulo');
+
+require_once('../config/lang/eng.php');
+require_once('../tcpdf.php');
+require_once('../fpdi/src/fpdi.php');
+
+$arquivoPDF = 'seu_arquivo_aqui.pdf';
+$paginas = array(1,2,3,4,5,6,7,8,9,10);
+try {
+  createNewPDF($paginas,$arquivoPDF);
+} catch (Exception $e) {
+	print "Erro: ".$e;
+}
+
+function createNewPDF($paginas,$arquivoPDF){
+	$pdf = new FPDI();
+	$pagecount 	= $pdf->setSourceFile($arquivoPDF);
+	for($i=1;$i<=$pagecount;$i++){
+		if(in_array($i, $paginas)){
+			$tplidx 	= $pdf->importPage($i, '/MediaBox');
+			$pdf->addPage();
+			$pdf->useTemplate($tplidx, 10, 10, 90);
+		}
+	}
+	$pdf->Output('newpdf-xxxxx.pdf', 'D');
+}
